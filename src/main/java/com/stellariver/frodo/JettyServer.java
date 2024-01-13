78e705d47c75fa8b371d56ed86294dcb29d0c8b5
houchuang <houchuang@outlook.com>
2024-01-13 19:57:25 +0800

    basic ability complete

--- src/main/java/com/stellariver/frodo/JettyServer.java
+++ src/main/java/com/stellariver/frodo/JettyServer.java
@@ -11,16 +11,26 @@ import org.eclipse.jetty.util.log.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JettyServer {
 
     private static final Logger logger = Log.getLogger(Server.class);
+
+    private static final JdkCompiler jdkCompiler = new JdkCompiler();
+
+    static private final Pattern scriptPattern = Pattern.compile("public\\s+class\\s+[a-zA-Z0-9_]+\\s+implements Callable<String>\\s*\\{");
 
     static {
         CompletableFuture.runAsync(() -> {
@@ -35,7 +45,8 @@ public class JettyServer {
                                    HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException {
                     request.setHandled(true);
                     byte[] bytes = IOUtils.toByteArray(httpRequest.getInputStream());
-                    Charset charset = Charset.forName(httpRequest.getCharacterEncoding());
+                    String rawCharset = httpRequest.getCharacterEncoding();
+                    Charset charset = StringUtil.isBlank(rawCharset) ? StandardCharsets.UTF_8 : Charset.forName(rawCharset);
                     CharsetDecoder decoder = charset.newDecoder();
                     ByteBuffer buf = ByteBuffer.wrap(bytes);
                     String requestBody;
@@ -47,15 +58,9 @@ public class JettyServer {
                         return;
                     }
 
-                    try {
-                        String responseBody = process(requestBody);
-                        httpResponse.setContentType("application/json;charset=" + charset.name());
-                        httpResponse.setCharacterEncoding(charset.name());
-                        httpResponse.getWriter().write(responseBody);
-                    } catch (Throwable throwable) {
-                        logger.warn(throwable);
-                        httpResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                    }
+                    httpResponse.setContentType("application/json;charset=" + charset.name());
+                    httpResponse.setCharacterEncoding(charset.name());
+                    httpResponse.getWriter().write(process(requestBody));
 
                 }
             });
@@ -73,15 +78,43 @@ public class JettyServer {
 
     }
 
-
+    @SuppressWarnings({"unchecked", "ResultOfMethodCallIgnored"})
     static public String process(String request) {
-        return request + request;
+
+        Matcher matcher = scriptPattern.matcher(request);
+
+        if (matcher.find()) {
+            matcher.group();
+        } else {
+            return "could not find class implements Callable<String> in \n\n" + wrapper(request);
+        }
+
+        if (matcher.find()) {
+            return "find too many public class in \n\n" + wrapper(request);
+        }
+
+        try {
+            Class<?> clazz = jdkCompiler.compile(request);
+            Class<Callable<String>> callableClass = (Class<Callable<String>>) clazz;
+            Callable<String> callable = callableClass.newInstance();
+            return callable.call();
+        } catch (Throwable throwable) {
+            return printable(throwable);
+        }
+
     }
 
+    static private String wrapper(String code) {
+        return "\\*------------------------------------------------------------------------*\\\n\n"
+                + code
+                + "\n\n\\*------------------------------------------------------------------------*\\\n";
+    }
 
-
-    public static void main(String[] args) throws IOException {
-        System.in.read();
+    static private String printable(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
     }
 
 }
